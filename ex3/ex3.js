c12d3754826a6093af19430f755d0dce3e6eeae1
Frank Parejo <francisco.parejo.lopez@gmail.com>
2018-11-14 15:32:36 +0100

    ex3 solution.

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,16 +1,41 @@
-function increment(x) { return x + 1; }
-function decrement(x) { return x - 1; }
-function double(x) { return x * 2; }
-function half(x) { return x / 2; }
+function increment(x) {
+  return x + 1;
+}
+function decrement(x) {
+  return x - 1;
+}
+function double(x) {
+  return x * 2;
+}
+function half(x) {
+  return x / 2;
+}
 
-function compose() {}
-function pipe() {}
+function pipe(...fns) {
+  return function(...args) {
+    return fns.reduce((reduction, fn) => fn(reduction), args);
+  };
+}
 
-var f = compose(decrement,double,increment,half);
-var p = pipe(half,increment,double,decrement);
+function compose(...fns) {
+  return pipe(...fns.reverse());
+}
 
-f(3) === 4;
+var f = compose(
+  decrement,
+  double,
+  increment,
+  half
+);
+var p = pipe(
+  half,
+  increment,
+  double,
+  decrement
+);
+
+console.log(f(3) === 4);
 // true
 
-f(3) === p(3);
+console.log(f(3) === p(3));
 // true
