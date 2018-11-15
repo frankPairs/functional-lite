6c0f27e0cb1997c2c0bb4648cc6ac7efeefe4517
Frank Parejo <francisco.parejo.lopez@gmail.com>
2018-11-15 16:39:10 +0100

    ex4 solution.

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,13 +1,25 @@
 function lotteryNum() {
-	return (Math.round(Math.random() * 100) % 58) + 1;
+  return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber(){}
+function pickNumber(arr) {
+  var newArr = arr;
+  var num = null;
+
+  do {
+    num = lotteryNum();
+  } while (newArr.includes(num));
+
+  newArr.push(num);
+  newArr.sort((x, y) => x > y);
+
+  return newArr;
+}
 
 var luckyLotteryNumbers = [];
 
 for (var i = 0; i < 6; i++) {
-	pickNumber();
+  luckyLotteryNumbers = pickNumber(luckyLotteryNumbers);
 }
 
 console.log(luckyLotteryNumbers);
