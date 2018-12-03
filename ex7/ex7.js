8657a7d5ed8166c0cb3e4426c6c6161273d4d84c
Frank Parejo <francisco.parejo.lopez@gmail.com>
2018-12-03 23:15:47 +0100

    complete ex7 solution.

--- ex7/ex7.js
+++ ex7/ex7.js
@@ -1,4 +1,4 @@
-function num(n) {
+function constant(n) {
   return function() {
     return n;
   };
@@ -12,7 +12,7 @@ function add2(fn1, fn2) {
   return add(fn1(), fn2());
 }
 
-function addnLoop(...fns) {
+function addnLoop(fns) {
   while (fns.length > 2) {
     const [fn0, fn1, ...rest] = fns;
 
@@ -22,7 +22,7 @@ function addnLoop(...fns) {
   return add2(fns[0], fns[1]);
 }
 
-function addnRecursion(fnResult, fn, ...fns) {
+function addnRecursion([fnResult, fn, ...fns]) {
   if (fn === undefined) {
     return fnResult();
   }
@@ -31,13 +31,32 @@ function addnRecursion(fnResult, fn, ...fns) {
     return add2(fnResult, fn);
   }
 
-  return addnRecursion(() => add2(fnResult, fn), ...fns);
+  return addnRecursion([() => add2(fnResult, fn), ...fns]);
 }
 
-function addnArrayHelpers(...fns) {
+function addnArrayHelpers(fns) {
   return fns.reduce((fn0, fn1) => () => add2(fn0, fn1))();
 }
 
-console.log(addnLoop(num(10), num(20), num(5)));
-console.log(addnRecursion(num(10), num(20), num(5), num(4)));
-console.log(addnArrayHelpers(num(10), num(20), num(5), num(4)));
+const arrNumbers = [1, 2, 2, 5, 7, 7, 9, 21, 54, 232, 28, 39, 39];
+
+console.log(addnLoop([constant(10), constant(20), constant(5)]));
+console.log(
+  addnRecursion([constant(10), constant(20), constant(5), constant(4)])
+);
+console.log(
+  addnArrayHelpers([constant(10), constant(20), constant(5), constant(4)])
+);
+
+console.log(
+  addnRecursion(
+    arrNumbers
+      .reduce(
+        (uniqueArr, num) =>
+          uniqueArr.includes(num) ? uniqueArr : uniqueArr.concat(num),
+        []
+      )
+      .filter(num => num % 2 === 0)
+      .map(num => constant(num))
+  )
+);
